cac20e4097162e456407b8006860ad2e5ed66ce6
aw0474 <aw0474@126.com>
2022-08-19 14:21:52 +0800

    第er次提交

---
+++ src/main/java/com/offcn/git/FitDemo.java
@@ -0,0 +1,13 @@
+package com.offcn.git;
+
+/**
+ * @Auther:BigData-aw
+ * @ClassName:FitDemo
+ * @功能描述:
+ * @Version:1.0
+ */
+public class FitDemo {
+    public static void main(String[] args) {
+        System.out.println("second commit");
+    }
+}
