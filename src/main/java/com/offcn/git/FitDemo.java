65f75f268224f56d525519994eec64d9db14fc03
aw0474 <aw0474@126.com>
2022-08-19 14:26:20 +0800

    第san次提交

--- src/main/java/com/offcn/git/FitDemo.java
+++ src/main/java/com/offcn/git/FitDemo.java
@@ -9,5 +9,6 @@ package com.offcn.git;
 public class FitDemo {
     public static void main(String[] args) {
         System.out.println("second commit");
+        System.out.println("hot-fix");
     }
 }
